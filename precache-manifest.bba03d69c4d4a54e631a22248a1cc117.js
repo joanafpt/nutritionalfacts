1d760ff31cb88b1d85c3eb96f91a99deb8ce1e11
Joana Teixeira <joanajteixeira@outlook.com>
2021-03-25 14:31:31 +0000

    Updates

---
+++ precache-manifest.bba03d69c4d4a54e631a22248a1cc117.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eadce2c0b5bb4c754ff0c56cd99e1a6c",
+    "url": "/nutritionalfacts/index.html"
+  },
+  {
+    "revision": "2314ae36bd4d2814cb1c",
+    "url": "/nutritionalfacts/static/css/main.b371d1f4.chunk.css"
+  },
+  {
+    "revision": "662e4291124e9ab76256",
+    "url": "/nutritionalfacts/static/js/2.82fe28cc.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/nutritionalfacts/static/js/2.82fe28cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2314ae36bd4d2814cb1c",
+    "url": "/nutritionalfacts/static/js/main.e450448b.chunk.js"
+  },
+  {
+    "revision": "86f52beeef204ba773ba",
+    "url": "/nutritionalfacts/static/js/runtime-main.1f2eeb7c.js"
+  }
+]);
